2964c836834edcf8a5e14c62fe9e79037a49f4bd
Tim Roeder <timothyjroeder@gmail.com>
2022-04-20 11:22:11 -0600

    Run: add required files, ensure file close and comments

--- parse.rb
+++ parse.rb
@@ -3,31 +3,32 @@
 class Parse
   Header = Struct.new(:magic_string, :version, :number_of_records)
   attr_accessor :binary, :header, :records
-
+  
   def initialize(binary)
     @binary  = binary
     @header  = self.parse_mps7_header
     @records = self.create_records
   end
-
+  
   def parse_mps7_header
     header_row = @binary.read(9).unpack('A4CN')
-    raise 'Invalid MPS7 Header String Format' unless header_row[0] == 'MPS7'
-    Header.new(header_row[0], header_row[1], header_row[2])
+    raise 'Invalid MPS7 Header String Format' unless header_row.first == 'MPS7'
+    Header.new(*header_row)
   end
-
+  
   def create_records
+    record_types = %i[credit debit start_autopay end_autopay]
     @records = []
     @header.number_of_records.times do
       unless @binary.eof?
-        record_type = @binary.read(1).unpack('C').first
+        record_type = record_types.fetch(@binary.read(1).unpack('C').first)
         case record_type
-        when 0, 1
+        when :credit, :debit
           string = @binary.read(20).unpack('NQ>G')
-        when 2, 3
+        when :start_autopay, :end_autopay
           string = @binary.read(12).unpack('NQ>')
         end
-        @records << Record.new(record_type, string[0], string[1], string[2])
+        @records << Record.new(record_type, *string)
       end
     end
 
